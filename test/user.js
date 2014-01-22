496194ab2fd58f66f63d2474bceb85c2c7978653
Andrew Shaffer <andrew@weo.io>
2014-01-22 13:47:21 -0800

    add unit test for login

--- test/user.js
+++ test/user.js
@@ -1,11 +1,25 @@
-describe('test', function() {
+describe('User controller', function() {
   require('./helpers/boot.js')();
-  it('test2', function(done) {
-    request
-      .post('/user/login')
-      .send({username: 'josh', password: 'test'})
-      .end(function(err, res) {
-        done();
-      });
+
+  describe('login', function() {
+    it('Should handle non-existent username', function(done) {
+      request
+        .post('/user/login')
+        .send({username: 'badusername', password: 'test'})
+        .end(function(err, res) {
+          var status = res.status
+            , body = res.body;
+
+          expect(status).to.equal(404);
+          expect(body.message).to.equal('User not found');
+          expect(body.errors).to.be.an('array');
+          expect(body.errors).to.have.length(1);
+          expect(body.errors[0]).to.equal({
+            resource: 'User',
+            field: 'username',
+            code: 'missing'
+          });
+        });
+    });
   });
 });
