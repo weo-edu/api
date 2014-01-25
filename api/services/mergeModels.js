6cbc42c57081f43e643554420dd010a7da803e8d
Andrew Shaffer <andrew@weo.io>
2014-01-24 19:23:45 -0800

    add mergeModels.js file

---
+++ api/services/mergeModels.js
@@ -0,0 +1,11 @@
+module.exports = function(/* ... */) {
+  var args = _.toArray(arguments);
+  args.unshift({});
+  args.push(function(a, b) {
+    return _.isArray(a)
+      ? b
+      : undefined;
+  });
+
+  return _.merge.apply(_, args);
+};
