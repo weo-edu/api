fe6aaf8fefaeb79bc920d7b892dbb09be9d6782b
Andrew Shaffer <andrew@weo.io>
2014-03-11 16:05:14 -0700

    remove log

--- api/services/redis.js
+++ api/services/redis.js
@@ -1,7 +1,6 @@
 var redis = require('redis')
   , client;
 
-console.log('REDIS', process.env.REDISTOGO_URL);
 if(process.env.REDISTOGO_URL) {
   var parsed = require('url').parse(process.env.REDISTOGO_URL)
     , parts = parsed.auth.split(':');
