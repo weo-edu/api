615843f094e06ae42f68e6e373d4c08519207cff
Andrew Shaffer <andrew@weo.io>
2014-03-11 16:00:44 -0700

    test

--- api/services/redis.js
+++ api/services/redis.js
@@ -1,6 +1,7 @@
 var redis = require('redis')
   , client;
 
+console.log('REDIS', process.env.REDISTOGO_URL);
 if(process.env.REDISTOGO_URL) {
   var parsed = require('url').parse(process.env.REDISTOGO_URL)
     , parts = parsed.auth.split(':');
