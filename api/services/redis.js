e633c3788fd946dc9910dd51667c4b4e00ce50e9
Andrew Shaffer <andrew@weo.io>
2014-03-26 17:54:36 -0700

    fix redis hostname issue

--- api/services/redis.js
+++ api/services/redis.js
@@ -1,7 +1,7 @@
 var redis = require('redis');
 var config = require('../../config/redis').redis;
 
-var client = redis.createClient(config.port, config.hostname);
+var client = redis.createClient(config.port, config.host);
 if(config.password)
   client.auth(config.password);
 
--- config/redis.js
+++ config/redis.js
@@ -2,4 +2,3 @@ var redisUrl = process.env.REDISTOGO_URL || 'redis://@localhost:6379/0';
 var parseRedisUrl = require('parse-redis-url')();
 
 module.exports.redis = parseRedisUrl.parse(redisUrl);
-console.log('redis', module.exports.redis);
