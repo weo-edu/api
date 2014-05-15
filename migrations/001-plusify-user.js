7253de5c7a7819635b40a77b693a385f1db2e971
Josh Taylor <joshrtay@gmail.com>
2014-05-15 11:19:28 -0700

    migrations and procfile

---
+++ migrations/001-plusify-user.js
@@ -0,0 +1,26 @@
+var chug = require('chug');
+var _ = require('lodash');
+
+
+var map = {
+  type: 'userType', 
+  name: 'displayName'
+  title: 'name.honorificPrefix',
+  full_name: 'name.formatted',
+  first_name: 'name.givenName',
+  last_name: 'name.familyName'
+};
+
+exports.up = function(next){
+  chug.src('user', {})
+    .pipe(chug.transform(map))
+    .pipe(chug.dest, 'users')
+    .on('end', next);
+};
+
+exports.down = function(next){
+  chug.src('users', {})
+    .pipe(chug.transform(_.invert(map)))
+    .pipe(chug.dest, 'user')
+    .on('end', next);
+};
