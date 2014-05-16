f66dec2fad0d6c74ce07186ed4612daf12242fe3
Josh Taylor <joshrtay@gmail.com>
2014-05-15 19:08:35 -0700

    add mongo restore

--- bin/sync.js
+++ bin/sync.js
@@ -10,8 +10,17 @@ var local = mongoURI.parse(process.env.MONGOHQ_URL)
 
 console.log('cwd', process.cwd());
 
-spawn('./bin/mongodump', [
+var mongodump = spawn('./bin/mongodump', [
   '-h', remote.hosts[0].host + ':' + remote.hosts[0].port, 
   '-d', remote.database, 
   '-u', remote.username,
   '-p', remote.password], {stdio: 'inherit'});
+
+mongodump.on('exit', function() {
+  spawn('./bin/mongorestore', [
+  '-h', local.hosts[0].host + ':' + local.hosts[0].port, 
+  '-d', local.database, 
+  '-u', local.username,
+  '-p', local.password
+  'dump/'], {stdio: 'inherit'});
+});
