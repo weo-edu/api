4309a0a73af126a9399ea7d216d8ea16fe190fe0
Josh Taylor <joshrtay@gmail.com>
2014-05-15 18:59:35 -0700

    add bin

--- bin/sync.js
+++ bin/sync.js
@@ -2,22 +2,16 @@
 
 var mongo = require('mongodb');
 var mongoURI = require('mongodb-uri');
+var spawn = require('child_process').spawn;
 
 
 var remote = mongoURI.parse(process.env.MONGO_PRODUCTION_URL);
-var local = mongoURI.parse(process.env.MONGOHQ_URL);
+var local = mongoURI.parse(process.env.MONGOHQ_URL)
 
-mongo.MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
-  if (err) throw err;
+console.log('cwd', process.cwd());
 
-  db.admin().command({
-    copydb: 1,
-    fromhost: remote.hosts[0].host + ':' + remote.hosts[0].port,
-    fromdb: remote.database,
-    todb: local.database,
-    username: remote.username,
-    password: remote.password
-  }, function (err, res) {
-    console.log('sync', err, res);
-  });
-})
+spawn('./bin/mongodump', [
+  '-h', remote.hosts[0].host + ':' + remote.hosts[0].port, 
+  '-d', remote.database, 
+  '-u', remote.username,
+  '-p', remote.password], {stdio: 'inherit'});
