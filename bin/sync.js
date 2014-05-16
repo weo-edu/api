73ad539cb26efcca4a2586a986d6d0f2ded04306
Josh Taylor <joshrtay@gmail.com>
2014-05-15 18:38:50 -0700

    mongo native copy

--- bin/sync.js
+++ bin/sync.js
@@ -2,17 +2,22 @@
 
 var mongo = require('mongodb');
 var mongoURI = require('mongodb-uri');
-var spawn = require('child_process').spawn;
 
 
 var remote = mongoURI.parse(process.env.MONGO_PRODUCTION_URL);
 var local = mongoURI.parse(process.env.MONGOHQ_URL);
 
-console.log('local', local);
-console.log('remote', remote);
+mongo.MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
+  if (err) throw err;
 
-
-var mongodump = spawn('mongodump', [], {stdio: 'inherit'});
-mongodump.on('error', function(err) {
-  console.log('error', err.stack);
-});
+  db.admin().command({
+    copydb: 1,
+    fromhost: remote.hosts[0].host + ':' remote.hosts[0].port,
+    fromdb: remote.database,
+    todb: local.database,
+    username: remote.username,
+    password: remote.password
+  }, function (err, res) {
+    console.log('sync', err, res);
+  });
+})
