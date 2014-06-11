2a119ace72f3e601d17461cffc2326804d0db839
Andrew Shaffer <andrew@weo.io>
2014-06-10 18:30:06 -0700

    upgrade chug, add group migration

---
+++ migrations/003-group.js
@@ -0,0 +1,29 @@
+var chug = require('chug')(process.env.MONGOHQ_URL);
+var _ = require('lodash');
+var es = require('event-stream');
+var access = require('../lib/access');
+
+exports.up = function(next){
+  chug.src('groups', {})
+    .pipe(es.through(function(doc) {
+      if(! doc.status)
+        doc.status = 'active';
+      this.emit('data', doc);
+    }))
+    .pipe(es.through(function(doc) {
+      if(! doc.access || ! doc.access.allow.length) {
+        doc.access.allow = [
+          access.entry('public', 'teacher'),
+          access.entry('group', 'student', doc.id)
+        ];
+      }
+
+      this.emit('data', doc);
+    }))
+    .pipe(chug.dest('groups'))
+    .on('end', next);
+};
+
+exports.down = function(next){
+  next();
+};
