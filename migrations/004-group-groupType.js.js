ab5577d4956e671658f4ee967c769344c20b52be
Andrew Shaffer <andrew@weo.io>
2014-06-10 19:18:25 -0700

    upgrade migrate

---
+++ migrations/004-group-groupType.js.js
@@ -0,0 +1,22 @@
+var chug = require('chug')(require('../lib/config').mongo.url);
+var _ = require('lodash');
+var es = require('event-stream');
+
+exports.up = function(next){
+  chug.src('groups', {})
+    .pipe(es.through(function(doc) {
+      if(doc.groupType.indexOf(':') !== -1) {
+        var parts = doc.groupType.split(':');
+        doc.groupType = parts[0];
+        doc.status = parts[1].slice(0, -1);
+      }
+
+      this.emit('data', doc);
+    }))
+    .pipe(chug.dest('groups'))
+    .on('end', next);
+};
+
+exports.down = function(next){
+  next();
+};
