3066100f6d353fded8e2c9f0da811d0a7d2e5ea4
Michael Weintraub <mweint@gmail.com>
2014-08-29 17:59:19 -0700

    fixed markdown test

--- test/markdown.js
+++ test/markdown.js
@@ -5,6 +5,6 @@ require('./helpers/boot');
 describe('Markdown tests', function() {
   it('should have hashtags (pound symbol) disabled', function() {
     expect(markdown('#test')).to.equal('<p>#test</p>\n');
-    expect(markdown('test\n====')).to.equal('<h1 id="test">test</h1>\n');
+    expect(markdown('test\n====')).to.equal('<h1 id="md-header-test">test</h1>\n');
   });
 });
