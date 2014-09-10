64f8ee7c8d442fc2c14cb6c09fe52e8fa73be3cf
Josh Taylor <joshrtay@gmail.com>
2014-09-10 12:32:44 -0700

    add libs

---
+++ test/helpers/email.js
@@ -0,0 +1,32 @@
+var request = require('superagent');
+var cookie = require('cookie');
+
+
+var connect = null;
+exports.get = function(property, cb) {
+  var r = request
+    .get('http://tempmail.weo.io');
+
+  if (connect)
+    r.set('cookie', cookie.serialize('connect.sid', connect['connect.sid']));
+
+  r.end(function(err, res){
+    if (res.headers['set-cookie'])
+      connect = cookie.parse(res.headers['set-cookie'][0]);
+    cb && cb(err, res.body[property]);
+  });
+};
+
+exports.pollInbox = function(cb) {
+  exports.get('items', function(err, items) {
+    if (err) return cb(err);
+    if (items.length)
+      cb(null, items)
+    else {
+      setTimeout(function() {
+        exports.pollInbox(cb);
+      }, 1000);
+    }
+  })
+} 
+
