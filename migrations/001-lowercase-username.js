3642ca3da511c62293c6c4ea90c29b83109fd0d8
Andrew Shaffer <andrew@weo.io>
2014-10-13 19:19:48 -0700

    add migrate for lowercase usernames

--- migrations/001-lowercase-username.js
+++ migrations/001-lowercase-username.js
@@ -1,14 +1,10 @@
 var chug = require('chug')(require('../lib/config/').mongo.url);
-var _ = require('lodash');
 var es = require('event-stream');
 
 exports.up = function(next){
   chug.src('users', {})
     .pipe(es.through(function(doc) {
-      if(doc.email === 'jb.whittenburg@gmail.com')
-        doc.username = 'jobenw';
-      if(doc.email === 'alexw@thewillows.org')
-        doc.username = 'alexw';
+      doc.username = doc.username.toLowerCase();
       this.emit('data', doc);
     }))
     .pipe(chug.dest('users'))
