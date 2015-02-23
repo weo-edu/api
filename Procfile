87fec4343f1763f83149a5798c369642bec80a24
Andrew Shaffer <andrew@weo.io>
2015-02-23 15:36:25 -0800

    update procfile and add postinstall hooks for symlinking

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: node server.js
-migrate: ln -s `pwd`/lib node_modules/lib; npm run migrate;
-sync: ./bin/sync.js
+migrate: npm run migrate
+sync: node bin/sync.js
--- server.js
+++ server.js
@@ -1,33 +1,3 @@
 var debug = require('debug')('weo:pre-boot');
-var fs    = require('fs');
-var path  = require('path');
-
-debug('symlinking lib folder...');
-var lib = 'node_modules/lib';
-
-try {
-  var stat = fs.lstatSync(lib);
-  if(stat.isSymbolicLink()) {
-    debug('unlinking symlink');
-    fs.unlinkSync(lib);
-  } else {
-    debug('deleting folder');
-    fs.rmdirSync(lib);
-  }
-} catch(e) {
-  debug("lib folder symlink didn't exist...creating it");
-}
-
-
-fs.symlinkSync(path.join(process.cwd(), 'lib'), lib, 'dir');
-
-try {
-  fs.mkdirSync('assets');
-} catch (e) {
-  // do nothing if folder already exists
-}
-
-
 module.exports = require('lib/boot');
-
 debug('finish boot');
