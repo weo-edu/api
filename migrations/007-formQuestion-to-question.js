3f82448abc876181999a16ecd942f4e561d30fcf
Andrew Shaffer <darawk@gmail.com>
2015-06-16 19:42:48 -0700

    make migrations more fault tolerant

--- migrations/007-formQuestion-to-question.js
+++ migrations/007-formQuestion-to-question.js
@@ -4,16 +4,16 @@ var qs = require('querystring');
 var async = require('async');
 
 exports.up = function(next){
-  var n = 0;
   chug.src('shares')
     .pipe(es.through(function(doc) {
-      var attachments = doc._object[0].attachments;
-      attachments && attachments.forEach(function(object) {
-        if(object.objectType === 'formQuestion')
-          object.objectType = 'question';
-      });
+      if(doc._object && doc._object[0]) {
+        var attachments = doc._object[0].attachments;
+        attachments && attachments.forEach(function(object) {
+          if(object.objectType === 'formQuestion')
+            object.objectType = 'question';
+        });
+      }
 
-      n++;
       this.emit('data', doc);
     }))
     .on('error', function(err) {
--- migrations/014-scrape-media.js
+++ migrations/014-scrape-media.js
@@ -9,25 +9,28 @@ var types = ['link', 'video', 'image', 'document'];
 exports.up = function(next){
   chug.src('shares', {})
     .pipe(es.map(function(doc, cb) {
-      var arr = doc._object[0].attachments || [];
-      var self = this;
+      if(doc._object && doc._object[0]) {
+        var arr = doc._object[0].attachments || [];
+        var self = this;
 
-      if (!arr.length)
-        return cb(null, doc);
+        if (!arr.length)
+          return cb(null, doc);
 
-      async.each(arr, function(att, done) {
-        if(att.originalContent && types.indexOf(att.objectType) !== -1) {
-          scrape(att.originalContent, function(err, data) {
-            if (err) return done();
-            data.originalContent = att.originalContent;
-            _.extend(att, data);
+        async.each(arr, function(att, done) {
+          if(att.originalContent && types.indexOf(att.objectType) !== -1) {
+            scrape(att.originalContent, function(err, data) {
+              if (err) return done();
+              data.originalContent = att.originalContent;
+              _.extend(att, data);
+              done();
+            });
+          } else
             done();
-          });
-        } else
-          done();
-      }, function(err) {
-        cb(null, doc);
-      });
+        }, function(err) {
+          cb(null, doc);
+        });
+      } else
+        cb(null, doc)
 
     }))
     .pipe(chug.dest('shares'))
