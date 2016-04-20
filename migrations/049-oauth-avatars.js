1731aa3557cd4883b21ab2df761718bacc0a123f
Andrew Shaffer <andrew@weo.io>
2016-04-19 17:04:44 -0700

    store fb avatars on our own server and add migration for old accounts

---
+++ migrations/049-oauth-avatars.js
@@ -0,0 +1,93 @@
+/**
+ * Imports
+ */
+
+var chug = require('mongo-chug')(require('../lib/config/').mongo)
+var es = require('event-stream')
+var request = require('superagent')
+var uuid = require('uuid')
+var s3config = require('../lib/config').s3.uploads
+var s3client = require('knox').createClient(s3config)
+var bucketUrl = 'http://' + s3config.bucket + '.s3-' + s3config.region + '.amazonaws.com'
+var mongo = require('../lib/mongo')
+
+/**
+ * Migrations
+ */
+
+exports.up = function(next){
+  var ps = es.pause()
+  chug
+    .src('users', {'auth.facebook.id': {$exists: true}})
+    .pipe(ps)
+    .pipe(es.map(function(doc, cb) {
+      ps.pause()
+      console.log('processing user', doc._id)
+      process(doc, function (err) {
+        if (err) return cb(err)
+        cb(null, doc)
+        ps.resume()
+      }, cb)
+    }))
+    .pipe(chug.dest('users'))
+    .on('end', next)
+
+  ps.resume()
+}
+
+exports.down = function(next){
+  next()
+}
+
+/**
+ * Process a user document
+ */
+
+function process (user, cb) {
+  var avatars = mongo.collection('avatars')
+  console.log('process', user._id)
+  try {
+    avatars.findOne({user: user._id}).then(function (doc) {
+      if (!doc) return cb(null)
+      if (doc.url && doc.url.indexOf('fbcdn.net/hprofile') === -1) return cb()
+
+      s3upload('http://graph.facebook.com/' + user.auth.facebook.id + '/picture?type=large', function (err, url) {
+        if (err) return cb(err)
+        console.log('uploaded', user._id, user.auth.facebook.id, url)
+        avatars
+          .update({user: user._id}, {$set: {url: url}})
+          .then(function () { cb() }, cb)
+      })
+    }, cb)
+  } catch (e) {
+    console.log("CAUGHT", e)
+  }
+}
+
+/**
+ * Helpers
+ */
+
+function s3upload (url, cb) {
+  request
+    .get(url, function (err, res) {
+      if (err) return cb(err)
+
+      var path = '/' + uuid.v4()
+      var headers = {
+        'Content-Length': res.headers['content-length'],
+        'Content-Type': res.headers['content-type']
+      }
+
+      s3client.putBuffer(res.body, path, headers, function (err, res) {
+        if (err) return cb(err)
+        res
+          .resume()
+          .on('error', function (err) { cb(err) })
+          .on('end', function () {
+            cb(null, bucketUrl + path)
+          })
+      })
+    })
+    .on('error', function (err) { cb(err) })
+}
