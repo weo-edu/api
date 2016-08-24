b7665f51fcf100e7c77aff853702363659888614
Andrew Shaffer <andrew@weo.io>
2016-08-24 12:35:54 -0700

    add migration to ensure object.displayName is set

---
+++ migrations/051-object-displayNames.js
@@ -0,0 +1,47 @@
+var mongo = require('../lib/mongo')
+var es = require('event-stream')
+var strip = require('strip')
+
+var katexMathMLRegEx = /<math>.*?<\/math>/g
+
+exports.up = function(cb){
+  var map = {shares: 'Share', groups: 'Group', users: 'User'}
+
+  mongo.connect.then(function () {
+    var shares = mongo.collection('shares')
+
+    shares
+      .find()
+      .pipe(es.through(function (doc) {
+        console.log('doc', doc._id)
+        if (doc._object && doc._object[0] && doc._object[0].attachments) {
+          doc._object[0].attachments.forEach(function (att) {
+            if (!att.displayName && att.content) att.displayName = xf(att.content)
+
+            if (att.attachments) {
+              att.attachments.forEach(function (sub) {
+                if (!sub.displayName && sub.content) sub.displayName = xf(sub.content)
+              })
+            }
+          })
+        }
+
+        this.emit('data', doc)
+      }))
+      .pipe(es.map(function (doc, cb) {
+        shares.findOne(doc._id).update(doc).then(function () {
+          cb()
+        }, function (err) { cb(err) })
+      }))
+      .on('end', cb)
+  })
+}
+
+exports.down = function(next){
+  next()
+}
+
+
+function xf (str) {
+  return strip(str.replace(katexMathMLRegEx, ''))
+}
