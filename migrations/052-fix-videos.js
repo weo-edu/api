544f992f8bce48b8abd9326f67b2bc4a1f3e2e04
Andrew Shaffer <andrew@weo.io>
2016-09-15 18:49:05 -0700

    add better reporting to fix videos migrate

--- migrations/052-fix-videos.js
+++ migrations/052-fix-videos.js
@@ -13,26 +13,26 @@ exports.up = function (cb){
       .find()
       .pipe(es.map(function (doc, cb) {
         if (doc._object && doc._object[0] && doc._object[0].attachments) {
+          var found = false
           Promise.all(doc._object[0].attachments.map(function (att) {
             if ((att.objectType === 'video' || att.objectType === 'document') && att.content.startsWith('<p>') && att.originalContent) {
               return new Promise(resolve => {
                 scrape(att.originalContent, function (err, data) {
                   if (!err) _.extend(att, data)
+                  found = true
                   resolve()
                 })
               })
             }
 
             return new Promise(resolve => resolve())
-          })).then(() => cb(null, doc))
+          }))
+            .then(() => found && shares.findOne(doc._id).update(doc))
+            .then(() => cb())
+            .then(() => found && console.log('updated', doc._id))
         } else {
           cb(null, doc)
         }
-      }))
-      .pipe(es.map(function (doc, cb) {
-        shares.findOne(doc._id).update(doc).then(function () {
-          cb()
-        }, function (err) { cb(err) })
       }))
       .on('end', cb)
   })
